6b19068b7bcbe86f2ec4c89d37fa40f1c2ed4195
rohmer <rohmer@yahoo.com>
2015-04-03 11:05:26 -0700

    Adding config

---
+++ PiRRDaemon/Configuration.cpp
@@ -0,0 +1,43 @@
+#include "Configuration.h"
+
+Configuration::Configuration()
+{
+	configFile = "config.json";
+	configParsed = parseConfig();
+}
+
+Configuration::Configuration(string configurationFile)
+{	
+	configFile = configurationFile;
+	configParsed = parseConfig();
+}
+
+bool Configuration::parseConfig()
+{
+	string jsonText;
+	try
+	{
+		std::ifstream inFile;
+		inFile.open(configFile.c_str());
+		if (!inFile)
+		{
+			return false;
+		}
+		
+		// Read file into a string
+		inFile.seekg(0, std::ios::end);
+		jsonText.reserve(inFile.tellg());
+		inFile.seekg(0, std::ios::beg);
+		jsonText.assign((std::istreambuf_iterator<char>(inFile)),
+			std::istreambuf_iterator<char>());
+		inFile.close();
+	}
+	catch (exception e)
+	{
+		return false;
+	}
+
+	// File is read, lets parse
+	JSONNODE *node = json_parse(jsonText.c_str());
+
+}
--- PiRRDaemon/Configuration.h
+++ PiRRDaemon/Configuration.h
@@ -2,10 +2,23 @@
 
 #include <log4cplus/logger.h>
 #include <libjson.h>
+#include <fstream>
+#include <string>
+#include <streambuf>
 
-class Configruation
+using namespace std;
+using namespace log4cplus;
+
+class Configuration
 {
+	private:
+		Logger logger;
+		string configFile;
+		bool configParsed;
+
+		bool parseConfig();
+
 	public:
-		Logger Logger;
-	
-}
+		Configuration();
+		Configuration(string configFile);
+};
